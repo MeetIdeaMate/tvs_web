3cc922dbf9bbb7e63e883433c7b3d4490c0ed87b
ajithtechlambdas <ajith@techlambdas.com>
2024-05-16 17:10:46 +0530

    add .gitignore

---
+++ lib/view/voucher_receipt/voucher_receipt_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class VoucherReceiptList extends StatefulWidget {
+  const VoucherReceiptList({super.key});
+
+  @override
+  State<VoucherReceiptList> createState() => _VoucherReceiptListState();
+}
+
+class _VoucherReceiptListState extends State<VoucherReceiptList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(children: [
+
+    ],),);
+  }
+}
